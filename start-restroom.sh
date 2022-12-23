46e12378aeeb4c488f99ddc5ff8fad57216a8c84
Alberto Lerda <albertolerda97@gmail.com>
2022-12-23 23:13:03 +0100

    change files_dir

--- start-restroom.sh
+++ start-restroom.sh
@@ -2,9 +2,10 @@
 
 RESTROOM_CONTAINER_NAME=$1
 CONTRACTS="$2"
+FILES="$3"
 
 echo "Starting restroom instance"
 echo "Using contracts in $CONTRACTS"
-docker run --name $RESTROOM_CONTAINER_NAME --publish 3000:3000 -v "$CONTRACTS":"/app/contracts" --detach ghcr.io/albertolerda/restroom-github-action:main
-sleep 7
+export FILES_DIR="/var/files"
+docker run --name $RESTROOM_CONTAINER_NAME --publish 3000:3000 -e FILES_DIR -v "$FILES":"/var/files" -v "$CONTRACTS":"/app/contracts" --detach ghcr.io/dyne/restroom-mw:master
 
