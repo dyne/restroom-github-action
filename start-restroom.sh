1cf6fecd7f9c710f7da2c1c8163fefacc840936b
Alberto Lerda <albertolerda97@gmail.com>
2022-12-23 23:23:04 +0100

    wait the service to start

--- start-restroom.sh
+++ start-restroom.sh
@@ -8,4 +8,4 @@ echo "Starting restroom instance"
 echo "Using contracts in $CONTRACTS"
 export FILES_DIR="/var/files"
 docker run --name $RESTROOM_CONTAINER_NAME --publish 3000:3000 -e FILES_DIR -v "$FILES":"/var/files" -v "$CONTRACTS":"/app/contracts" --detach ghcr.io/dyne/restroom-mw:master
-
+sleep 7
